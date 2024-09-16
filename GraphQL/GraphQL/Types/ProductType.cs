93e2bca68bf9e1e0a2bc94f2710838420cce9179
Santiago Gutierrez <santi0gutierrez0@gmail.com>
2024-09-16 20:19:05 -0300

    Added ProductType class for GraphQL

---
+++ GraphQL/GraphQL/Types/ProductType.cs
@@ -0,0 +1,18 @@
+using HotChocolate.Types;
+using OrderManager.Models;
+
+namespace OrderManager.GraphQL.Types
+{
+    public class ProductType : ObjetctType<Product>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Product> descriptor)
+        {
+            descriptor.Description("Representa un producto en el sistema.");
+
+            descriptor.Field(p => p.Id).Description("Identificador único del producto.");
+
+            descriptor.Field(p => p.Name).Description("Nombre del producto.")
+        }
+    }
+}
+    
