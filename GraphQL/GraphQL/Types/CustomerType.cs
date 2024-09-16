160983b91d1a646b2b57cb274eba05913a394c91
Santiago Gutierrez <santi0gutierrez0@gmail.com>
2024-09-16 20:15:22 -0300

    Added CustomerType class for GraphQL

---
+++ GraphQL/GraphQL/Types/CustomerType.cs
@@ -0,0 +1,17 @@
+using HotChocolate.Types;
+using OrderManager.Models;
+
+namespace OrderManager.GraphQL.Types
+{
+    public class CustomerType : ObjetctType<Customer>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Customer> descriptor)
+        {
+            descriptor.Description("Representa un cliente en el sistema.");
+
+            descriptor.Field(c => c.Id).Description("Identificador único del cliente.");
+
+            descriptor.Field(c => c.FullName).Description("Nombre completo del cliente.");
+        }
+    }
+}
