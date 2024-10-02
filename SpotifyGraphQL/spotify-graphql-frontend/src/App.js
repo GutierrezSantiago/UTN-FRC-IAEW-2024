988b003bc845e9a4f09c104189d0fbe3b04e7f40
Santiago Gutierrez <santi0gutierrez0@gmail.com>
2024-10-01 21:14:33 -0300

    Updated App.js to config Auth0 and Apollo Provider

--- SpotifyGraphQL/spotify-graphql-frontend/src/App.js
+++ SpotifyGraphQL/spotify-graphql-frontend/src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { ApolloProvider } from "@apollo/client";
+import { useAuth0 } from "@auth0/auth0-react";
+import createApolloClient from "./ApolloClient";
+import ArtistSearch from './ArtistSearch';
 
 function App() {
+  const { isAuthenticated, getAccessTokenSilently, isLoading } = useAuth0();
+  const [client, setClient] = React.useState(null);
+  React.useEffect(() => {
+    const initApollo = async () => {
+      if (isAuthenticated) {
+        try {
+          const token = await getAccessTokenSilently();
+          const ApolloClient = createApolloClient(token);
+          setClient(ApolloClient);
+        } catch (error) {
+          console.error("Error obteniendo el token de Auth0: ", error);
+        }
+      }
+    }
+    initApollo();
+  }, [isAuthenticated, getAccessTokenSilently]);
+
+  if (isLoading) return <div>Loading...</div>;
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isAuthenticated ? (
+        client ? (
+          <ApolloProvider client={client}>
+            <h1>
+              Spotify Artist Search
+            </h1>
+            <ArtistSearch />
+          </ApolloProvider>
+        ) : (
+          <div>
+            Loading Apollo Client...
+          </div>
+        )
+      ) : (
+        <div>
+          Please log in.
+        </div>
+      )}
     </div>
   );
 }
